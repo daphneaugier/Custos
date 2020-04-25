dd9431a443019036a4c752186e1a9ced8819be6c
Daphne Augier <35980232+daphneaugier@users.noreply.github.com>
2020-04-25 16:21:34 +0200

    Home screen with functional ARM Button

--- app/src/main/java/com/example/custos/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/custos/ui/home/HomeFragment.kt
@@ -1,14 +1,20 @@
 package com.example.custos.ui.home
 
+import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.custos.R
+import com.example.custos.R.*
 
 class HomeFragment : Fragment() {
 
@@ -21,7 +27,26 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(layout.fragment_home, container, false)
+
+        val armButton: Button = root.findViewById(R.id.buttonARM)
+        val lockIcon: ImageView = root.findViewById(R.id.lockIcon)
+
+        armButton.setOnClickListener {
+            if ( armButton.text == "DISARM"){
+                armButton.text = "ARM"
+                lockIcon.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        getActivity()!!.getApplicationContext(),
+                        R.drawable.ic_lock))
+            }else{
+                armButton.text = "DISARM"
+                lockIcon.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        getActivity()!!.getApplicationContext(),
+                        R.drawable.ic_unlock))
+            }
+        }
 
 
         return root
